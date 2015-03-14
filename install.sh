379745b282eadb05a71a3eb587e86b7d1f5ca32d
Christopher Valles <hello@christophervalles.com>
2015-03-14 16:52:54 -0600

    Evolved stuff

---
+++ install.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+#bash
+echo "Setting up bash..."
+ln -s ~/.dotfiles/bash/bash_profile ~/.bash_profile
+
+# git
+echo "Setting up git..."
+ln -s ~/.dotfiles/git/gitconfig ~/.gitconfig
+ln -s ~/.dotfiles/git/gitignore_global ~/.gitignore_global
+
+#tmux
+echo "Setting up tmux..."
+ln -s ~/.dotfiles/tmux/tmux.conf ~/.tmux

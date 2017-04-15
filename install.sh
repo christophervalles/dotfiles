51520aef5610cecea8c3769ec0f9ed84d564b943
Christopher Valles <hello@christophervalles.com>
2017-04-15 19:54:10 +0100

    Adding i3 stuff

--- install.sh
+++ install.sh
@@ -8,7 +8,3 @@ ln -s ~/.dotfiles/zsh/zshrc ~/.zshrc
 echo "Setting up git..."
 ln -s ~/.dotfiles/git/gitconfig ~/.gitconfig
 ln -s ~/.dotfiles/git/gitignore_global ~/.gitignore_global
-
-#slate
-echo "Setting up slate..."
-ln -s ~/.dotfiles/slate/slate ~/.slate

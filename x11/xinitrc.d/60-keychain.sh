13a893eea8b334123b54fb649bbf6559eb030bfd
Chris Valles <tbhot3ww@meraki.com>
2018-05-24 06:23:22 +0100

    WIP

--- x11/xinitrc.d/60-keychain.sh
+++ x11/xinitrc.d/60-keychain.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-keychain ~/.ssh/id_rsa
+keychain --eval --quiet ~/.ssh/id_rsa
 [ -f ~/.keychain/$HOSTNAME-sh ] && . ~/.keychain/$HOSTNAME-sh 2>/dev/null
 [ -f ~/.keychain/$HOSTNAME-sh-gpg ] && . ~/.keychain/$HOSTNAME-sh-gpg 2>/dev/null
